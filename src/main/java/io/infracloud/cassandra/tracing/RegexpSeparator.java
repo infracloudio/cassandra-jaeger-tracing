b41af9e70d676d637766f835e51aa121ad106e27
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-05-16 01:29:20 +0200

    licenses

--- src/main/java/io/infracloud/cassandra/tracing/RegexpSeparator.java
+++ src/main/java/io/infracloud/cassandra/tracing/RegexpSeparator.java
@@ -1,3 +1,20 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.infracloud.cassandra.tracing;
 
 import io.opentracing.Span;
@@ -57,7 +74,7 @@ public class RegexpSeparator {
     };
 
     static public AnalysisResult match(String trace) {
-        for (SingleRegexp srp : regexps) {
+        for (final SingleRegexp srp : regexps) {
             final Matcher match = srp.match(trace);
             if (match.matches()) {
                 return new RegexpResult(srp, match);
@@ -108,7 +125,7 @@ public class RegexpSeparator {
 
         @Override
         public void applyTags(Span span) {
-            for (String groupName : srp.namedGroups) {
+            for (final String groupName : srp.namedGroups) {
                 span.setTag(groupName, match.group(groupName));
             }
         }
