e4c1cbd64338df3684388bed5f06ace7106a8c65
Piotr Maślanka <piotr.maslanka@henrietta.com.pl>
2020-05-16 01:17:08 +0200

    more entries for RegexpSeparator

--- src/main/java/io/infracloud/cassandra/tracing/RegexpSeparator.java
+++ src/main/java/io/infracloud/cassandra/tracing/RegexpSeparator.java
@@ -1,9 +1,7 @@
 package io.infracloud.cassandra.tracing;
 
 import io.opentracing.Span;
-import org.apache.commons.math3.analysis.function.Sin;
 
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -13,12 +11,69 @@ import java.util.regex.Pattern;
  */
 public class RegexpSeparator {
 
+    final static private SingleRegexpSeparator[] regexes = {
+            new SingleRegexpSeparator("Key cache hit",
+                    "Key cache hit for sstable (?<sstableid>\\d+)",
+                    new String[]{"sstableid"}),
+            new SingleRegexpSeparator("Parsing query",
+                    "Parsing (?<query>.*)",
+                    new String[]{"query"}),
+            new SingleRegexpSeparator("Reading data",
+                    "reading data from /(?<othernode>.*)",
+                    new String[]{"othernode"}),
+            new SingleRegexpSeparator("Read live rows and tombstone cells",
+                    "Read (?<liverows>\\d+) live rows and (?<tombstonecells>\\d+) tombstone cells",
+                    new String[]{"liverows", "tombstonecells"}),
+            new SingleRegexpSeparator("Merged data from memtables and sstables",
+                    "Merged data from memtables and (?<sstables>\\d+) sstables",
+                    new String[]{"sstables"}),
+            new SingleRegexpSeparator("Skipped non-slice-intersecting sstables",
+                    "Skipped (?<howmany>\\d+/\\d+) non-slice-intersecting sstables, included (?<tombstones>\\d+) due to tombstones",
+                    new String[]{"howmany", "tombstones"}),
+            new SingleRegexpSeparator("Received READ message",
+                    "READ message received from /(?<othernode>.*)",
+                    new String[]{"othernode"}),
+            new SingleRegexpSeparator("Enqueuing response",
+                    "Enqueuing response to /(?<othernode>.*)",
+                    new String[]{"othernode"}),
+            new SingleRegexpSeparator("Sending response",
+                    "Sending REQUEST_RESPONSE message to /(?<othernode>.*)",
+                    new String[]{"othernode"}),
+            new SingleRegexpSeparator("REQUEST_RESPONSE message received",
+                    "REQUEST_RESPONSE message received from /(?<othernode>.*)",
+                    new String[]{"othernode"}),
+            new SingleRegexpSeparator("Sending READ message",
+                    "Sending READ message to /(?<othernode>.*)",
+                    new String[]{"othernode"}),
+            new SingleRegexpSeparator("Scanned rows and matched ",
+                    "Scanned (?<rows>\\d+) rows and matched (?<matched>\\d+)",
+                    new String[]{"rows", "matched"}),
+            new SingleRegexpSeparator("Partition index found",
+                    "Partition index with (?<entries>\\d+) found for sstable (?<sstableid>\\d+)",
+                    new String[]{"entries", "sstableid"}),
+            new SingleRegexpSeparator("Speculating read retry",
+                    "speculating read retry on /(?<othernode>.*)",
+                    new String[]{"othernode"})
+    };
+
+    static protected AnalysisResult match(String trace) {
+        for (SingleRegexpSeparator srp : regexes) {
+            Matcher match = srp.match(trace);
+            if (match.matches()) {
+                return new RegexpResult(srp, match);
+            }
+        }
+        return new NoMatch(trace);
+    }
+
     abstract static public class AnalysisResult {
         /**
          * Return the name of the trace to use in Jaeger
          */
         abstract public String getTraceName();
-        public void applyTags(Span span) {}
+
+        public void applyTags(Span span) {
+        }
     }
 
     public static class NoMatch extends AnalysisResult {
@@ -27,6 +82,7 @@ public class RegexpSeparator {
         protected NoMatch(String trace) {
             this.trace = trace;
         }
+
         public String getTraceName() {
             return trace;
         }
@@ -70,47 +126,5 @@ public class RegexpSeparator {
         protected Matcher match(String trace) {
             return pattern.matcher(trace);
         }
-    }
-    final static private SingleRegexpSeparator [] regexes = {
-            new SingleRegexpSeparator("Key cache hit",
-                    "Key cache hit for sstable (?<sstableid>\\d+)",
-                    new String[]{"sstableid"}),
-            new SingleRegexpSeparator("Parsing query",
-                    "Parsing (?<query>.*)",
-                    new String[]{"query"}),
-            new SingleRegexpSeparator("Read live rows and tombstone cells",
-                    "Read (?<liverows>\\d+) live rows and (?<tombstonecells>\\d+) tombstone cells",
-                    new String[]{"liverows", "tombstonecells"}),
-            new SingleRegexpSeparator("Merged data from memtables and sstables",
-                    "Merged data from memtables and (?<sstables>\\d+) sstables",
-                    new String[]{"sstables"}),
-            new SingleRegexpSeparator("Skipped non-slice-intersecting sstables",
-                    "Skipped (?<howmany>\\d+/\\d+) non-slice-intersecting sstables, included (?<tombstones>\\d+) due to tombstones",
-                    new String[]{"howmany","tombstones"}),
-            new SingleRegexpSeparator("Received READ message",
-                    "READ message received from /(?<othernode>.*)",
-                    new String[]{"othernode"}),
-            new SingleRegexpSeparator("Enqueuing response",
-                    "Enqueuing response to /(?<othernode>.*)",
-                    new String[]{"othernode"}),
-            new SingleRegexpSeparator("Sending response",
-                    "Sending REQUEST_RESPONSE message to /(?<othernode>.*)",
-                    new String[]{"othernode"}),
-            new SingleRegexpSeparator("REQUEST_RESPONSE message received",
-                    "REQUEST_RESPONSE message received from /(?<othernode>.*)",
-                    new String[]{"othernode"}),
-            new SingleRegexpSeparator("Sending READ message",
-                    "Sending READ message to /(?<othernode>.*)",
-                    new String[]{"othernode"})
-    };
-
-    static protected AnalysisResult match(String trace) {
-        for (SingleRegexpSeparator srp : regexes) {
-            Matcher match = srp.match(trace);
-            if (match.matches()) {
-                return new RegexpResult(srp, match);
-            }
-        }
-        return new NoMatch(trace);
     }
 }
